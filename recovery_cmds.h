LineageOS/android_bootable_recovery

recovery: Add resize2fs, tune2fs to fstools
Change-Id: Ia8988dcce72601d4baf02596d3ff487f6aea26c6

recovery_cmds.h
@@ -35,6 +35,8 @@ int e2fsdroid_main(int argc, char **argv);
 
 int mke2fs_main(int argc, char **argv);
 int e2fsck_main(int argc, char **argv);
+int resize2fs_main(int argc, char **argv);
+int tune2fs_main(int argc, char **argv);
 
 int mkfs_f2fs_main(int argc, char **argv);
 int fsck_f2fs_main(int argc, char **argv);

ColtOS-Joyeuse-A10/platform_bootable_recovery

recovery_cmds.h
@@ -36,6 +36,8 @@ int e2fsdroid_main(int argc, char** argv);
 
 int mke2fs_main(int argc, char** argv);
 int e2fsck_main(int argc, char** argv);
+int resize2fs_main(int argc, char** argv);
+int tune2fs_main(int argc, char** argv);
 
 int mkfs_f2fs_main(int argc, char** argv);
 int fsck_f2fs_main(int argc, char** argv);