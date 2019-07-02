LineageOS/android_bootable_recovery

recovery: Add fstools
Support all filesystem tools via multi-call invocation.

 * ext4: mkfs.ext4, fsck.ext4, and alternative names

 * f2fs: mkfs.f2fs, fsck.f2fs

 * vfat: fsck_msdos

 * exfat: mkfs.exfat, fsck.exfat

 * ntfs: mkfs.ntfs, fsck.ntfs, mount.ntfs

 * sgdisk

 * Android specific tools e2fsdroid, sload.f2fs

Also remove TARGET_USERIMAGES_USE_F2FS guard for f2fs.  Building f2fs
does not really add that much to the recovery image size.

Change-Id: I69a55ad3048718112970fe0ca2d7d38a1aaacf52

recovery_cmds.h
@@ -30,6 +30,26 @@ int start_main(int argc, char **argv);
 int stop_main(int argc, char **argv);
 int mksh_main(int argc, char **argv);
 
+/* Filesystem tools */
+int e2fsdroid_main(int argc, char **argv);
+
+int mke2fs_main(int argc, char **argv);
+int e2fsck_main(int argc, char **argv);
+
+int mkfs_f2fs_main(int argc, char **argv);
+int fsck_f2fs_main(int argc, char **argv);
+
+int fsck_msdos_main(int argc, char **argv);
+
+int mkfs_exfat_main(int argc, char **argv);
+int fsck_exfat_main(int argc, char **argv);
+
+int mkfs_ntfs_main(int argc, char **argv);
+int fsck_ntfs_main(int argc, char **argv);
+int mount_ntfs_main(int argc, char **argv);
+
+int sgdisk_main(int argc, char **argv);
+
 #ifdef __cplusplus
 }
 #endif

ColtOS-Joyeuse-A10/platform_bootable_recovery

recovery_cmds.h
@@ -31,6 +31,26 @@ int start_main(int argc, char** argv);
 int stop_main(int argc, char** argv);
 int mksh_main(int argc, char** argv);
 
+/* Filesystem tools */
+int e2fsdroid_main(int argc, char** argv);
+
+int mke2fs_main(int argc, char** argv);
+int e2fsck_main(int argc, char** argv);
+
+int mkfs_f2fs_main(int argc, char** argv);
+int fsck_f2fs_main(int argc, char** argv);
+
+int fsck_msdos_main(int argc, char** argv);
+
+int mkfs_exfat_main(int argc, char** argv);
+int fsck_exfat_main(int argc, char** argv);
+
+int mkfs_ntfs_main(int argc, char** argv);
+int fsck_ntfs_main(int argc, char** argv);
+int mount_ntfs_main(int argc, char** argv);
+
+int sgdisk_main(int argc, char** argv);
+
 #ifdef __cplusplus
 }
 #endif